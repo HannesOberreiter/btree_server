4f602ef76bba49a70c2d583c30b73151830e6445
HannesOberreiter <hoberreiter@gmail.com>
2021-03-26 14:27:24 +0100

    fixed guard middleware

--- src/api/middlewares/guard.middleware.ts
+++ src/api/middlewares/guard.middleware.ts
@@ -1,10 +1,8 @@
 import { authenticate } from 'passport';
-import { promisify } from 'es6-promisify';
 import { forbidden, badRequest } from '@hapi/boom';
 
-import { User } from '@models/user.model';
 import { ROLES } from '@enums/role.enum';
-import { list } from '@utils/enum.util';
+import { listNumber } from '@utils/enum.util';
 import { IUserRequest } from '@interfaces/IUserRequest.interface';
 import { IResponse } from '@interfaces/IResponse.interface';
 
@@ -23,20 +21,17 @@ export class Guard {
    *
    * @param roles
    */
-  static authorize = (roles = list(ROLES)) => (req: IUserRequest, res: IResponse, next: (e?: Error) => void): void => authenticate( 'jwt', { session: false }, Guard.handleJWT(req, res, next, roles) )(req, res, next);
+  static authorize = (roles = listNumber(ROLES)) => (req: IUserRequest, res: IResponse, next: (e?: Error) => void): void => authenticate( 'jwt', { session: false }, Guard.handleJWT(req, res, next, roles) )(req, res, next);
 
-  private static handleJWT = (req: IUserRequest, res: IResponse, next: (error?: Error) => void, roles: string) => async (err: Error, user: any, info: any) => {
+  private static handleJWT = (req: IUserRequest, res: IResponse, next: (error?: Error) => void, roles: number[]) => async (err: Error, user: any, info: any) => {
 
     const error = err || info;
 
     if (error || !user) {
       return next( badRequest(info?.name) );
     } 
-    
-    console.log(user);
-    console.log(req.body);
 
-    if (roles === ROLES.admin && user.rank !== ROLES.admin && parseInt(req.params.bee_id, 10) !== user.bee_id ) {
+    if (roles === [ROLES.admin] && user.rank !== ROLES.admin && parseInt(req.params.bee_id, 10) !== user.bee_id ) {
       return next( forbidden('Forbidden area') );
     } else if (!roles.includes(user.rank)) {
       return next( forbidden('Forbidden area') );
@@ -44,7 +39,7 @@ export class Guard {
       return next( badRequest(err?.message) );
     }
 
-    req.payload = user;
+    req.user = user;
 
     return next();
   }
