2166603f913a917720532d4a579a03af14dcb122
HannesOberreiter <hoberreiter@gmail.com>
2021-03-31 07:50:41 +0200

    removed https we do it with ngix

--- src/servers/http.server.ts
+++ src/servers/http.server.ts
@@ -28,10 +28,6 @@ export class HTTPServer {
    * 
    */
   private options = {
-    credentials: {
-      key: ssl.isActive === 1 ? readFileSync(ssl.key, "utf8") : null,
-      cert: ssl.isActive === 1 ? readFileSync(ssl.cert, "utf8") : null,
-    },
     port: port
   }
 
@@ -44,7 +40,7 @@ export class HTTPServer {
    */
   start(): void {
     try {
-      const server = ssl.isActive === 1 ? createServer(this.options.credentials, this.app) : this.app;
+      const server = this.app;
       this.http = server.listen(port, function() {
         if(env !== ENVIRONMENT.test) {
           Container.resolve('Logger').log('info', `HTTP(S) server is now running on port ${port} (${env})`, { label: 'Server' } );
