58825f974f74fb46fc1afb2b0794e9ab21ab558a
HannesOberreiter <hoberreiter@gmail.com>
2021-03-31 08:03:55 +0200

    removed unused packages

--- src/config/container.config.ts
+++ src/config/container.config.ts
@@ -8,7 +8,6 @@ import { AuthController } from "@controllers/auth.controller";
 import { HiveController } from "@controllers/hive.controller";
 import { OptionController } from "@controllers/options.controller";
 
-import { Can } from "@services/can.service";
 import { Logger } from "@services/logger.service";
 import { ProxyRouter } from "@services/proxy-router.service";
 export class Container {
@@ -31,7 +30,6 @@ export class Container {
       .register({ UserController : awilix.asClass(UserController).singleton() })
       .register({ AuthController : awilix.asClass(AuthController).singleton() })
       .register({ Logger : awilix.asClass(Logger).singleton() })   
-      .register({ Can : awilix.asClass(Can).singleton() })     
       .register({ ProxyRouter : awilix.asClass(ProxyRouter).singleton() });      
 
     return this.container;
