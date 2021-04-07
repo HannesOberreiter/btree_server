7596f9b8f439927bc24bba6c9bbf9f8a64740409
HannesOberreiter <hoberreiter@gmail.com>
2021-04-07 10:45:07 +0200

    created rearing models

---
+++ src/api/models/rearing/rearing.model.ts
@@ -0,0 +1,68 @@
+import { Model } from 'objection';
+import { Company } from '@models/company.model';
+import { RearingDetail } from './rearing_detail.model';
+import { RearingType } from './rearing_type.model';
+
+export class Rearing extends Model {
+  id!: number;
+  larvae!: number;
+  hatch!: number;
+  note!: string;
+  date!: Date;
+  type_id!: number;
+  detail_id!: number;
+  user_id!: number;
+
+  company?: Company;
+  start?: RearingDetail;
+  type?: RearingType;
+
+  constructor() {
+    super();
+  }
+
+  static tableName = 'rearings';
+  static idColumn = 'id';
+
+  static jsonSchema = {
+    type: 'object',
+    required: [],
+    properties: {
+      id: { type: 'integer' },
+      larvae: { type: 'integer' },
+      hatch: { type: 'integer' },
+      date: { type: 'date-time' },
+      note: { type: 'string', maxLength: 2000 },
+      type_id: { type: 'integer' },
+      detail_id: { type: 'integer' },
+      user_id: { type: 'integer' }
+    }
+  };
+
+  static relationMappings = () => ({
+    company: {
+      relation: Rearing.HasOneRelation,
+      modelClass: Company,
+      join: {
+        from: ['rearings.user_id'],
+        to: ['company.id']
+      }
+    },
+    start: {
+      relation: Rearing.HasOneRelation,
+      modelClass: RearingDetail,
+      join: {
+        from: ['rearings.detail_id'],
+        to: ['rearing_details.id']
+      }
+    },
+    type: {
+      relation: Rearing.HasOneRelation,
+      modelClass: RearingType,
+      join: {
+        from: ['rearings.type_id'],
+        to: ['rearing_types.id']
+      }
+    },
+  });
+}
---
+++ src/api/models/rearing/rearing_step.model.ts
@@ -0,0 +1,46 @@
+import { Model } from 'objection';
+import { RearingDetail } from '@models/rearing/rearing_detail.model';
+import { RearingType } from './rearing_type.model';
+
+export class RearingStep extends Model {
+  id!: number;
+  type_id!: number;
+  detail_id!: number;
+  position!: number;
+
+  type?: RearingType;
+  detail?: RearingDetail;
+
+  static tableName = 'rearing_steps';
+  static idColumn = 'id';
+
+  static jsonSchema = {
+    type: 'object',
+    required: ['position'],
+    properties: {
+      id: { type: 'integer' },
+      type_id: { type: 'integer' }, // RearingType FK
+      detail_id: { type: 'integer' }, // RearingDetail FK
+      position: { type: 'integer' } // Order of Rearing Steps for Rearing
+    }
+  };
+
+  static relationMappings = () => ({
+    detail: {
+      relation: RearingStep.BelongsToOneRelation,
+      modelClass: RearingDetail,
+      join: {
+        from: 'rearing_steps.detail_id',
+        to: 'rearing_details.id'
+      }
+    },
+    type: {
+      relation: RearingStep.BelongsToOneRelation,
+      modelClass: RearingType,
+      join: {
+        from: 'rearing_steps.type_id',
+        to: 'rearing_types.id'
+      }
+    }
+  });
+}
