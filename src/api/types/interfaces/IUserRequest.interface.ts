565bc9b5c23223848d95b95966e8e6d3838d384a
HannesOberreiter <hoberreiter@gmail.com>
2022-01-25 13:26:16 +0100

    push

--- src/api/types/interfaces/IUserRequest.interface.ts
+++ src/api/types/interfaces/IUserRequest.interface.ts
@@ -2,7 +2,7 @@ import { Request } from 'express';
 
 export interface IUserRequest extends Request {
   payload?: any;
-  body?: any;
+  body: any;
   user?: any;
   params: Record<string, string>;
 }
