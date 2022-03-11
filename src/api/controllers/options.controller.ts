25597fccd63e6f92ae13ab846ba5f22686da9ce1
HannesOberreiter <hoberreiter@gmail.com>
2022-03-11 14:54:03 +0100

    push options

--- src/api/controllers/options.controller.ts
+++ src/api/controllers/options.controller.ts
@@ -4,41 +4,46 @@ import { checkMySQLError } from '@utils/error.util';
 import { IUserRequest } from '@interfaces/IUserRequest.interface';
 import { ChargeType } from '@models/option/charge_type.model';
 import { CheckupType } from '@models/option/checkup_type.model';
-import dayjs from 'dayjs';
 import { FeedType } from '@models/option/feed_type.model';
 import { HarvestType } from '@models/option/harvest_type.model';
 import { TreatmentType } from '@models/option/treatment_type.model';
 import { TreatmentDisease } from '@models/option/treatment_disease.model';
 import { TreatmentVet } from '@models/option/treatment_vet.model';
+import { HiveSource } from '../models/option/hive_source.model';
+import { HiveType } from '../models/option/hive_type.mode';
+import { QueenMating } from '../models/option/queen_mating.model';
+import { QueenRace } from '../models/option/queen_race.model';
 export class OptionController extends Controller {
   constructor() {
     super();
   }
 
-  async getDropdowns(req: IUserRequest, res: Response, next) {
-    const types = [
-      ChargeType,
-      CheckupType,
-      FeedType,
-      HarvestType,
-      TreatmentDisease,
-      TreatmentType,
-      TreatmentVet
-    ];
-    let results = {};
-    for (let i of types) {
-      const result = await i
-        .query()
-        .where({ user_id: req.user.user_id, modus: 1 })
-        .orderBy([{ column: 'favorite', order: 'desc' }, { column: 'name' }]);
-      results[i.name] = result;
-    }
-    res.locals.data = {
-      data: results,
-      meta: {
-        timestamp: dayjs()
-      }
-    };
+  private static tables = {
+    charge_types: ChargeType,
+    hive_sources: HiveSource,
+    hive_types: HiveType,
+    feed_types: FeedType,
+    harvest_types: HarvestType,
+    checkup_types: CheckupType,
+    queen_matings: QueenMating,
+    queen_races: QueenRace,
+    treatment_diseases: TreatmentDisease,
+    treatment_types: TreatmentType,
+    treatment_vets: TreatmentVet
+  };
+
+  async get(req: IUserRequest, res: Response, next) {
+    console.log(req.params);
+    const table = Object(OptionController.tables)[req.params.table];
+    const result = await table
+      .query()
+      .where({ user_id: req.user.user_id })
+      .orderBy([
+        { column: 'modus' },
+        { column: 'favorite', order: 'desc' },
+        { column: 'name' }
+      ]);
+    res.locals.data = result;
     next();
   }
 }
--- src/api/routes/v1/option.route.ts
+++ src/api/routes/v1/option.route.ts
@@ -1,22 +1,19 @@
 import { Router } from '@classes/router.class';
 import { Container } from '@config/container.config';
-import { Validator } from '@middlewares/validator.middleware';
 import { Guard } from '@middlewares/guard.middleware';
 import { ROLES } from '@enums/role.enum';
+import { Validator } from '@/api/middlewares/validator.middleware';
 export class OptionRouter extends Router {
   constructor() {
     super();
   }
-
   define() {
-    // Get Task Dropdowns for QuickTool
-    // also returns a timestamp, as we don't need really to fetch it every time
-    // only if user edits any of the task dropdowns or after X minutes
     this.router
-      .route('/dropdowns')
+      .route('/:table')
       .get(
+        Validator.handleOption,
         Guard.authorize([ROLES.read, ROLES.admin, ROLES.user]),
-        Container.resolve('OptionController').getDropdowns
+        Container.resolve('OptionController').get
       );
   }
 }
