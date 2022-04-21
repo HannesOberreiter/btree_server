b810e13743506e38b6cb451915d831d3b21c1b99
HannesOberreiter <hoberreiter@gmail.com>
2022-04-21 14:21:27 +0200

    translations

--- src/api/utils/translations.util.ts
+++ src/api/utils/translations.util.ts
@@ -2,32 +2,32 @@ import { map } from 'lodash';
 
 const messageTranslation = {
   requiredField: {
-    de: 'test',
-    en: 'try'
+    de: 'Fehlendes verpflichtendes Feld.',
+    en: 'Missing required field.',
   },
   wrongLogin: {
     de: 'Falsches Passwort oder E-Mail.',
-    en: 'Wrong passwort or email.'
+    en: 'Wrong passwort or email.',
   },
   'Invalid value': {
     de: 'Falscher Wert',
-    en: 'Invalid value'
-  }
+    en: 'Invalid value',
+  },
 };
 
 const fieldTranslation = {
   password: {
     de: 'Passwort',
-    en: 'password'
+    en: 'password',
   },
   email: {
     de: 'E-Mail',
-    en: 'email'
+    en: 'email',
   },
   key: {
     de: 'Schlüssel',
-    en: 'Key'
-  }
+    en: 'Key',
+  },
 };
 
 const translateMessages = (errObj) => {
