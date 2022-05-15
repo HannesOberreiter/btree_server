1945a02bdd42e847096ac02edf39dfbedd5405ab
HannesOberreiter <hoberreiter@gmail.com>
2022-05-15 12:40:05 +0200

    typo

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -35,7 +35,7 @@ if (env.env === 'production') {
     return false;
   };
 } else if (env.env === 'staging') {
-  // This code is only used for migrating old databse to new schema
+  // This code is only used for migrating old database to new schema
 
   const userId = new findUser();
 
