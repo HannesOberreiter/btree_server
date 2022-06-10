93af646b9ef1b394d08862dbea7c2a225ce92421
HannesOberreiter <hoberreiter@gmail.com>
2022-06-10 11:30:15 +0200

    feat(alpha): :sparkles: building cron tasks

---
+++ src/api/utils/cron.util.ts
@@ -0,0 +1,230 @@
+import dayjs from "dayjs";
+import { Apiary } from "../models/apiary.model";
+import { Company } from "../models/company.model";
+import { CompanyBee } from "../models/company_bee.model";
+import { Hive } from "../models/hive.model";
+import { Movedate } from "../models/movedate.model";
+import { User } from "../models/user.model";
+import { checkMySQLError } from "./error.util";
+import { Dropbox } from "../models/dropbox.model";
+import { ChargeType } from "../models/option/charge_type.model";
+import { CheckupType } from "../models/option/checkup_type.model";
+import { FeedType } from "../models/option/feed_type.model";
+import { HarvestType } from "../models/option/harvest_type.model";
+import { HiveSource } from "../models/option/hive_source.model";
+import { HiveType } from "../models/option/hive_type.mode";
+import { LoginAttemp } from "../models/login_attempt.model";
+import { QueenMating } from "../models/option/queen_mating.model";
+import { QueenRace } from "../models/option/queen_race.model";
+import { RearingDetail } from "../models/rearing/rearing_detail.model";
+import { RearingType } from "../models/rearing/rearing_type.model";
+import { Rearing } from "../models/rearing/rearing.model";
+import { RearingStep } from "../models/rearing/rearing_step.model";
+import { RefreshToken } from "../models/refresh_token.model";
+import { Scale } from "../models/scale.model";
+import { ScaleData } from "../models/scale_data.model";
+import { TreatmentDisease } from "../models/option/treatment_disease.model";
+import { TreatmentType } from "../models/option/treatment_type.model";
+import { TreatmentVet } from "../models/option/treatment_vet.model";
+import { Feed } from "../models/feed.model";
+import { Treatment } from "../models/treatment.model";
+import { Checkup } from "../models/checkup.model";
+import { Harvest } from "../models/harvest.model";
+import { Queen } from "../models/queen.model";
+import { raw } from "objection";
+import { MailService } from "../services/mail.service";
+
+
+export const cleanupDatabase = async () => {
+  try {
+    const cleanup = {type: 'cleanup'};
+    // Delete data which is marked as "deleted" and older than one month
+    const lastMonth = dayjs().subtract(1, 'month').toISOString()
+    // Delete user if they did not login in the past 5 years and are the only users in a company
+    const timeToBeForgotten = dayjs().subtract(5, 'year').toISOString()
+
+    return await Company.transaction(async (trx) => {
+        
+        cleanup['CompanyBee'] = await CompanyBee.query(trx).delete().leftJoinRelated('company').leftJoinRelated('user').whereNull('company.id').orWhereNull('user.id');
+        cleanup['Company'] = await Company.query(trx).delete().leftJoinRelated('user').whereNull('user.id')
+        cleanup['User'] = await User.query(trx).delete().leftJoinRelated('company').whereNull('company.id')
+
+        const forgottenIds = await CompanyBee.query(trx).distinct('bee_id')
+            .leftJoinRelated('user')
+            .where('user.last_visit', '<=', timeToBeForgotten)
+            .groupBy('company_bee.user_id')
+            .having(raw('COUNT(bee_id)'), '=', 1)
+        cleanup['Forgotten'] = forgottenIds.length;
+        forgottenIds.forEach(async (v) => {
+            await User.query(trx).delete().findById(v.bee_id)
+        })
+
+        cleanup['LoginAttemp'] = await LoginAttemp.query(trx).delete().andWhere('time', '<=', lastMonth)
+        
+        cleanup['Apiary'] = await Apiary.query(trx).delete()
+            .leftJoinRelated('company')
+            .whereNull('company.id')
+            .orWhere('deleted', true)
+            .andWhere('deleted_at', '<=', lastMonth)
+        cleanup['Hive'] = await Hive.query(trx).delete()
+            .leftJoinRelated('company')
+            .whereNull('company.id')
+            .orWhere('deleted', true)
+            .andWhere('deleted_at', '<=', lastMonth)
+        cleanup['Movedate'] = await Movedate.query(trx).delete()
+            .leftJoinRelated('hive')
+            .leftJoinRelated('apiary')
+            .whereNull('hive.id')
+            .orWhereNull('apiary.id')
+        
+        cleanup['Dropbox'] = await Dropbox.query(trx).delete()
+            .leftJoinRelated('company')
+            .whereNull('company.id');
+        cleanup['charge_types'] = await ChargeType.query(trx).delete()
+            .leftJoinRelated('company')
+            .whereNull('company.id');
+        cleanup['checkup_types'] = await CheckupType.query(trx).delete()
+            .leftJoinRelated('company')
+            .whereNull('company.id');
+        cleanup['feed_types'] = await FeedType.query(trx).delete()
+            .leftJoinRelated('company')
+            .whereNull('company.id');
+        cleanup['harvest_types'] = await HarvestType.query(trx).delete()
+            .leftJoinRelated('company')
+            .whereNull('company.id');
+        cleanup['hive_sources'] = await HiveSource.query(trx).delete()
+            .leftJoinRelated('company')
+            .whereNull('company.id');
+        cleanup['hive_types'] = await HiveType.query(trx).delete()
+            .leftJoinRelated('company')
+            .whereNull('company.id');
+        cleanup['hive_types'] = await HiveType.query(trx).delete()
+            .leftJoinRelated('company')
+            .whereNull('company.id');
+        cleanup['queen_matings'] = await QueenMating.query(trx).delete()
+            .leftJoinRelated('company')
+            .whereNull('company.id');
+        cleanup['queen_races'] = await QueenRace.query(trx).delete()
+            .leftJoinRelated('company')
+            .whereNull('company.id');
+        cleanup['rearing_details'] = await RearingDetail.query(trx).delete()
+            .leftJoinRelated('company')
+            .whereNull('company.id'); 
+        cleanup['rearing_types'] = await RearingType.query(trx).delete()
+            .leftJoinRelated('company')
+            .whereNull('company.id');
+        cleanup['rearings'] = await Rearing.query(trx).delete()
+            .leftJoinRelated('company')
+            .whereNull('company.id');
+        cleanup['rearing_steps'] = await RearingStep.query(trx).delete()
+            .leftJoinRelated('type')
+            .leftJoinRelated('detail')
+            .whereNull('type.id')
+            .orWhereNull('detail.id');
+        cleanup['refresh_tokens'] = await RefreshToken.query(trx).delete()
+            .leftJoinRelated('company_bee')
+            .whereNull('company_bee.id');
+        cleanup['scales'] = await Scale.query(trx).delete()
+            .leftJoinRelated('company')
+            .whereNull('company.id');
+        cleanup['scale_data'] = await ScaleData.query(trx).delete()
+            .leftJoinRelated('scale')
+            .whereNull('scale.id');
+        cleanup['treatment_diseases'] = await TreatmentDisease.query(trx).delete()
+            .leftJoinRelated('company')
+            .whereNull('company.id');
+        cleanup['treatment_types'] = await TreatmentType.query(trx).delete()
+            .leftJoinRelated('company')
+            .whereNull('company.id');
+        cleanup['treatment_vets'] = await TreatmentVet.query(trx).delete()
+            .leftJoinRelated('company')
+            .whereNull('company.id');
+        cleanup['Feed'] = await Feed.query(trx).delete()
+            .leftJoinRelated('hive')
+            .whereNull('hive.id')
+            .orWhere('feeds.deleted', true)
+            .andWhere('feeds.deleted_at', '<=', lastMonth);
+        cleanup['Treatment'] = await Treatment.query(trx).delete()
+            .leftJoinRelated('hive')
+            .whereNull('hive.id')
+            .orWhere('treatments.deleted', true)
+            .andWhere('treatments.deleted_at', '<=', lastMonth);
+        cleanup['Checkup'] = await Checkup.query(trx).delete()
+            .leftJoinRelated('hive')
+            .whereNull('hive.id')
+            .orWhere('checkups.deleted', true)
+            .andWhere('checkups.deleted_at', '<=', lastMonth);
+        cleanup['Harvest'] = await Harvest.query(trx).delete()
+            .leftJoinRelated('hive')
+            .whereNull('hive.id')
+            .orWhere('harvests.deleted', true)
+            .andWhere('harvests.deleted_at', '<=', lastMonth);
+        cleanup['Queen'] = await Queen.query(trx).delete()
+            .leftJoinRelated('company')
+            .whereNull('company.id')
+            .orWhere('deleted', true)
+            .andWhere('deleted_at', '<=', lastMonth);
+        return cleanup;
+    });
+  } catch (e) {
+    throw checkMySQLError(e);
+  }
+};
+
+export const visReminder = async () => {
+    try {
+        const result = { type: 'vis_reminder' }
+        const checkDate = dayjs().add(2, 'day').format('YYYY-MM-DD')
+        const year = dayjs().year();
+        // Stichtag Zählung
+        const countDay1 = year + '-10-31';
+        const countDay2 = year + '-04-30';
+        // VIS Eingabe
+        const reportDay1 = year + '-12-31';
+        const reportDay2 = year + '-06-30';
+        if([countDay1, countDay2, reportDay1, reportDay2].includes(checkDate)){
+            const users = await User.query()
+                .select('username', 'email')
+                .where({
+                    'lang': 'de',
+                    'acdate': true
+                })
+
+            let mailDate, mailSubject;
+            switch (checkDate) {
+                case countDay1: 
+                    mailDate = countDay1;
+                    mailSubject = 'vis_count';
+                    break;
+                case countDay2: 
+                    mailDate = countDay2;
+                    mailSubject = 'vis_count';
+                    break;
+                case reportDay1: 
+                    mailDate = reportDay1;
+                    mailSubject = 'vis_count';
+                    break;
+                case reportDay2: 
+                    mailDate = reportDay2;
+                    mailSubject = 'vis_count';
+                    break;
+            }
+            result['mails'] = users.length;
+            const mailer = new MailService();
+            for(const i in users){
+                const user = users[i];
+                await mailer.sendMail({
+                    to: user.email,
+                    lang: 'de',
+                    subject: mailSubject,
+                    name: user.username ? user.username : 'false',
+                    key: mailDate
+                });
+            }
+        }
+        return result;
+    } catch (e) {
+        throw checkMySQLError(e);
+    }
+
+}
--- src/cron/scheduler.ts
+++ src/cron/scheduler.ts
@@ -1,9 +1,10 @@
 import cron from 'node-schedule';
 import { Container } from '@config/container.config';
+import { cleanupDatabase, visReminder } from '@/api/utils/cron.util';
 
 const task = {
   start: () => {
-    const rule = '0 */12 * * *';
+    const rule = '*/1 * * * *';
     Container.resolve('Logger').log(
       'info',
       `Test Cron-Job is starting with rule: ${rule}`,
@@ -15,10 +16,22 @@ const task = {
         rule: rule,
         tz: 'Europe/Vienna'
       },
-      function () {
-        Container.resolve('Logger').log('info', `Test Cron-Job`, {
-          label: 'CronJob'
-        });
+      async function () {
+        try {
+          Container.resolve('Logger').log('info', 'Test Cron-Job', {
+            label: 'CronJob'
+          });
+          const cleanup = await cleanupDatabase()
+          Container.resolve('Logger').log('info', JSON.stringify(cleanup), {
+            label: 'CronJob'
+          });
+          const vis_reminder = await visReminder()
+          Container.resolve('Logger').log('info', JSON.stringify(vis_reminder), {
+            label: 'CronJob'
+          });
+        } catch (e) {
+          Container.resolve('Logger').log('error', e, {label: 'CronJob'});
+        }
       }
     );
   },
