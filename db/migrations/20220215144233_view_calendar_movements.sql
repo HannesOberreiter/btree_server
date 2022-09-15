280fbf8832f62845219d78f8ac0d0bfed9d855f5
HannesOberreiter <hoberreiter@gmail.com>
2022-09-15 15:04:38 +0200

    fix(beta): :bug: movements were not grouped in calendar because of time

--- db/migrations/20220215144233_view_calendar_movements.sql
+++ db/migrations/20220215144233_view_calendar_movements.sql
@@ -1,19 +1,25 @@
 CREATE VIEW calendar_movements AS
-    SELECT
-		date, apiary_id, apiaries.name as apiary_name, apiaries.user_id,
-        GROUP_CONCAT(movedates.id) as move_ids,
-        GROUP_CONCAT(DISTINCT hive_id) as hive_ids,
-        GROUP_CONCAT(DISTINCT hives.name) as hive_names,
-        GROUP_CONCAT(DISTINCT created.email) as creators,
-        GROUP_CONCAT(DISTINCT edited.email) as editors
-    FROM movedates
-    LEFT JOIN hives
-        ON hives.id = movedates.hive_id
-    LEFT JOIN apiaries
-        ON apiaries.id = movedates.apiary_id
-    LEFT JOIN bees as created
-        ON created.id = movedates.bee_id
-    LEFT JOIN bees as edited
-        ON edited.id = movedates.edit_id
-    WHERE hives.deleted = 0 AND apiaries.deleted = 0
-    GROUP BY apiary_id, date, apiary_name, apiaries.user_id
+SELECT
+    DATE_FORMAT(date, '%Y-%m-%d') as date,
+    apiary_id,
+    apiaries.name as apiary_name,
+    apiaries.user_id,
+    GROUP_CONCAT(movedates.id) as move_ids,
+    GROUP_CONCAT(DISTINCT hive_id) as hive_ids,
+    GROUP_CONCAT(DISTINCT hives.name) as hive_names,
+    GROUP_CONCAT(DISTINCT created.email) as creators,
+    GROUP_CONCAT(DISTINCT edited.email) as editors
+FROM
+    movedates
+    LEFT JOIN hives ON hives.id = movedates.hive_id
+    LEFT JOIN apiaries ON apiaries.id = movedates.apiary_id
+    LEFT JOIN bees as created ON created.id = movedates.bee_id
+    LEFT JOIN bees as edited ON edited.id = movedates.edit_id
+WHERE
+    hives.deleted = 0
+    AND apiaries.deleted = 0
+GROUP BY
+    apiary_id,
+    DATE_FORMAT(date, '%Y-%m-%d'),
+    apiary_name,
+    apiaries.user_id
