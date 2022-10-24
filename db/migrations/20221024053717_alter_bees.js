b79204f18d08f23c100723d434b93f0176e4e359
HannesOberreiter <hoberreiter@gmail.com>
2022-10-24 07:43:08 +0200

    fix(dev): :fire: prevent error for already registered users with non unqiue usernames

---
+++ db/migrations/20221024053717_alter_bees.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.alterTable('bees', (t) => {
+    t.dropIndex('username', 'bees_username_unique');
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.alterTable('bees', (t) => {
+    t.unique('bees');
+  });
+};
