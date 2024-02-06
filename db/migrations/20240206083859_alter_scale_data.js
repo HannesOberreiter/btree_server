1c31cdb5b65c3ad27c03a5ec5c2652ba3a0b4e80
HannesOberreiter <hoberreiter@gmail.com>
2024-02-06 10:53:33 +0100

    fix(dev): :bug: timestamp migrations

---
+++ db/migrations/20240206083859_alter_scale_data.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export const up = function (knex) {
+  return knex.schema.alterTable('scale_data', (t) => {
+    t.dateTime('datetime').alter();
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export const down = function (knex) {
+  return knex.schema.alterTable('scale_data', (t) => {
+    t.timestamp('datetime').alter();
+  });
+};
